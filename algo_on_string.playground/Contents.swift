12cf64f46c92740da6edb2acecb09ef8ff59d986
Chandan Singh <chandan.ankush@gmail.com>
2022-06-02 21:05:40 +0530

    find max index

--- algo_on_string.playground/Contents.swift
+++ algo_on_string.playground/Contents.swift
@@ -33,5 +33,44 @@ func searchCountUsingIndexShifting(from input: String, searchParam: String) -> I
     return matchCount
 }
 
-let countReturned = searchCountUsingIndexShifting(from: "this is sample file", searchParam: " ")
-print(countReturned)
+//let countReturned = searchCountUsingIndexShifting(from: "this is sample file", searchParam: " ")
+//print(countReturned)
+
+
+//: search max matched words in given 2 string
+func findMaxMatchingString(from input1: String, and input2: String) -> String {
+    
+    guard !input1.isEmpty, !input2.isEmpty else {
+        return ""
+    }
+    
+    var maxMatchedWord = ""
+    
+    // node index is for  input index
+    var tempMatchedString = ""
+    var ni = 0
+    for inputChar in input1 {
+        
+        for nodeIndex in ni..<input2.count {
+            
+            let lookUpIndex = input2.index(input2.startIndex, offsetBy: nodeIndex)
+            let charAtIndex = input2[lookUpIndex]
+            if inputChar.lowercased() == charAtIndex.lowercased() {
+                tempMatchedString.append(charAtIndex)
+                ni += 1
+                break
+            } else {
+                if maxMatchedWord.count < tempMatchedString.count {
+                    maxMatchedWord = tempMatchedString
+                }
+                ni = 0
+                tempMatchedString = ""
+            }
+        }
+    }
+    return maxMatchedWord
+}
+
+
+let matchingString = findMaxMatchingString(from: "chandan chandan", and: "chandu chandan")
+print("matched max length is: \(matchingString)")
